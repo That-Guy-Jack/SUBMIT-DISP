1a32b12831cf4c53240e7a1b6c5bcc80ae481b83
Jack <Github@thatguyjack.co.uk>
2021-12-09 14:21:41 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 #it pull https://github.com/That-Guy-Jack/SUBMIT-DISP
 
-pyhton gunicorn --workers 3 --bind unix:myproject.sock -m 007 wsgi:app
+python gunicorn --workers 3 --bind unix:myproject.sock -m 007 wsgi:app
